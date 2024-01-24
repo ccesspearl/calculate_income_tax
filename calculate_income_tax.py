6628acac35f03e9e9fbd87c85407436f8d8fd153
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-24 08:08:30 +0800

    pseudocode

--- calculate_income_tax.py
+++ calculate_income_tax.py
@@ -7,3 +7,21 @@
 # Expected Output:
 # For example, suppose the taxable income is 45000, and the income tax payable is
 # 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+
+# Pseudocode 
+
+# Create a variable that will let the user input their income 
+# Create a variable for income tax payable and set it to 0 
+# Create an if statement for first 10000 taxable income 
+# Create an elif for second 10000 taxable income 
+# Create an else for remaining taxable income 
+# Print the results
+
+
+
+
+
+
+
+
